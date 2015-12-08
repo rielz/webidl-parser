07d300b79cf8ea4ed0fd98ff77cb2d4208d3a381
rielz <nope>
2015-12-08 19:04:18 +0100

    Use ES6 modules for web app

--- app/app.ts
+++ app/app.ts
@@ -1,33 +1,34 @@
-/// <reference path="../lib/tokenizer.d.ts" />
-/// <reference path="../lib/parser.d.ts" />
-/// <reference path="../lib/webidl.d.ts" />
-/// <reference path="../lib/typescript.d.ts" />
-
+/// <reference path="../src/tokenizer.ts" />
+/// <reference path="../src/parser.ts" />
+/// <reference path="../src/webidl.ts" />
+/// <reference path="../src/typescript.ts" />
 "use strict";
+
+import tokenize from "../src/tokenizer";
+import { TokenKind } from "../src/tokenizer";
+import { Writer, Emitter, Generator } from "../src/typescript";
+import * as webidl from "../src/webidl";
 
 window.addEventListener("load", (e) => {
 	const input = <HTMLTextAreaElement>document.querySelector("textarea#input");
 	const output = <HTMLTextAreaElement>document.querySelector("textarea#output");
 	
 	input.addEventListener("change", (e) => {
-		let iterable = tokenize(www.webidl.COMMON + input.value);
+		let iterable = tokenize(input.value);
 		let tokens = Array.from(iterable).filter(t => t.kind != TokenKind.whitespace && t.kind != TokenKind.comment);
-		
-		let parser = www.webidl.grammar.Definitions();
+
+		let parser = webidl.grammar.Definitions();
 		let definitions = parser(tokens);
 		definitions.success = definitions.success && definitions.rest.length === 1 && definitions.rest[0].kind === TokenKind.eof;
 
 		if(definitions.success) {
-			let generator = new microsoft.typescript.Generator();
+			let writer = new Writer();
+			let emitter = new Emitter(writer);
+			let generator = new Generator();
 			
-			let statements = definitions.value.map((def) => def.accept(generator)).reduce((list, item) => {
+			definitions.value.map((def) => def.accept(generator)).reduce((list, item) => {
 				return list.concat(item);
-			}, []);
-			
-			let writer = new microsoft.typescript.Writer();
-			let emitter = new microsoft.typescript.Emitter(writer);
-			
-			statements.forEach((stmt) => stmt.accept(emitter));
+			}, []).forEach((stmt) => stmt.accept(emitter));
 			
 			output.value = writer.toString();
 		} else {
